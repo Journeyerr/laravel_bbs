ae5030e11342d826b09eabf5c01b52bbccb6eab8
zhanganyuan <zhanganyuan@qiyiniu.cn>
2018-02-01 12:32:58 +0800

    finish basic layout

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+    <div class="container">
+        <p class="pull-left">
+            由 <a href="http://www.echophp.top" target="_blank">ZHANG AN YUAN</a> 孤岛的歌 <span style="color: #e27575;font-size: 14px;">❤</span>
+        </p>
+
+        <p class="pull-right"><a href="mailto:name@email.com">联系我们</a></p>
+    </div>
+</footer>
