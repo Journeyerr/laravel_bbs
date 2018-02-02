a5ed171c167b9b93124f42e8fadb1f1c04c55982
zhanganyuan <zhanganyuan@qiyiniu.cn>
2018-02-02 18:28:02 +0800

    创建话题加入编辑器

--- resources/views/topics/create_and_edit.blade.php
+++ resources/views/topics/create_and_edit.blade.php
@@ -1,7 +1,10 @@
 @extends('layouts.app')
 
-@section('content')
+@section('styles')
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/simditor.css') }}">
+@stop
 
+@section('content')
     <div class="container">
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
@@ -59,3 +62,19 @@
     </div>
 
 @endsection
+
+@section('scripts')
+    <script type="text/javascript"  src="{{ asset('js/module.min.js') }}"></script>
+    <script type="text/javascript"  src="{{ asset('js/hotkeys.min.js') }}"></script>
+    <script type="text/javascript"  src="{{ asset('js/uploader.min.js') }}"></script>
+    <script type="text/javascript"  src="{{ asset('js/simditor.min.js') }}"></script>
+
+    <script>
+        $(document).ready(function(){
+            var editor = new Simditor({
+                textarea: $('#editor'),
+            });
+        });
+    </script>
+
+@stop
