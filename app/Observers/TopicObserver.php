6c4c798b7cdcb2b9b5d3cf8a303ceec59369891f
zhanganyuan <zhanganyuan@qiyiniu.cn>
2018-02-03 15:06:55 +0800

    防止XSS攻击 编辑帖子

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -11,6 +11,11 @@ class TopicObserver
 {
     public function saving(Topic $topic)
     {
+
+        // 使用HTMLPurifier 过滤 xss 攻击， 在 config/purifier.php 下配置
+        $topic->body = clean($topic->body, 'user_topic_body');
+
+        // 使用 bootstrap/helpers.php 在创建话题时自动生成话题摘要
         $topic->excerpt = make_excerpt($topic->body);
     }
 }
--- app/Policies/TopicPolicy.php
+++ app/Policies/TopicPolicy.php
@@ -7,10 +7,9 @@ use App\Models\Topic;
 
 class TopicPolicy extends Policy
 {
-    public function update(User $user, Topic $topic)
+    public function update(User $crrentUser, Topic $topic)
     {
-        // return $topic->user_id == $user->id;
-        return true;
+        return $topic->user_id === $crrentUser->id;
     }
 
     public function destroy(User $user, Topic $topic)
