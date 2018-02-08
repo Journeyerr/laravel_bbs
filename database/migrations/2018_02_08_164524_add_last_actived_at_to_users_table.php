c997f608c8bf16cf7764bdd08b678314dd97467b
zhanganyuan <zhanganyuan@qiyiniu.cn>
2018-02-08 17:00:41 +0800

    增加用户最后登录时间字段，增加定时任务，redis数据入库

---
+++ database/migrations/2018_02_08_164524_add_last_actived_at_to_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddLastActivedAtToUsersTable extends Migration
+{
+    /**
+     * 增加 users 表字段：表示用户最后登录时间字段
+     *
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->timestamp('last_actived_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('last_actived_at');
+        });
+    }
+}
